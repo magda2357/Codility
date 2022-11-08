4796834dd512ce4a0ecb123a2b410050f419af11
MagdaSzut <magda.szut@gmail.com>
2022-11-08 22:29:40 +0100

    first attempt, rotation of values in array using arraycopy

---
+++ src/main/java/org/example/CyclicRotation.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class CyclicRotation {
+    public static int[] solution(int[] a, int k) {
+        int last;
+
+        if (a.length != 0) {
+            for (int i = 0; i < k; i++) {
+                last = a[a.length - 1];
+                System.arraycopy(a, 0, a, 1, a.length - 1);
+                a[0] = last;
+            }
+        }
+
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[]{1, 2, 3, 4, 5};
+        System.out.println("Przed przesunięciami: " + Arrays.toString(a));
+        System.out.println("Po przesunięciach: " + Arrays.toString(solution(a, 2)));
+    }
+}
