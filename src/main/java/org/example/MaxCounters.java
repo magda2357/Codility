fb797f74fa0b3d0847da8be782b990ec1023fb13
MagdaSzut <magda.szut@gmail.com>
2022-11-24 21:02:39 +0100

    two versions - time effectivness comparison fori to stream

--- src/main/java/org/example/MaxCounters.java
+++ src/main/java/org/example/MaxCounters.java
@@ -11,10 +11,10 @@ public class MaxCounters {
         for (int j : arr) {
 
             if (j <= n) {
-                result[j - 1] += 1;
                 if (result[j - 1] > max) {
                     max = result[j - 1];
                 }
+                result[j - 1] += 1;
             } else {
                 Arrays.fill(result, max);
             }
@@ -23,8 +23,34 @@ public class MaxCounters {
         return result;
     }
 
+    public static int[] countMax2(int n, int[] arr) {
+
+        int[] result = new int[n];
+
+        for (int j : arr) {
+
+            if (j <= n) {
+                result[j - 1] += 1;
+            } else {
+                Arrays.fill(result, Arrays.stream(result).max().getAsInt());
+            }
+
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
+        long startTime = System.nanoTime();
         System.out.println(Arrays.toString(countMax(5, new int[]{3, 4, 4, 6, 1, 4, 4})));
+        long endTime = System.nanoTime();
+        System.out.println(endTime - startTime);
+
+        long startTime2 = System.nanoTime();
+        System.out.println(Arrays.toString(countMax2(5, new int[]{3, 4, 4, 6, 1, 4, 4})));
+        long endTime2 = System.nanoTime();
+        System.out.println(endTime2 - startTime2);
+
+
         System.out.println(Arrays.toString(countMax(2, new int[]{1, 3})));
         System.out.println(Arrays.toString(countMax(1, new int[]{1})));
         System.out.println(Arrays.toString(countMax(1, new int[]{2, 1})));
