a9e20ed6d96bc6fc7bbf053cac9809bac722332d
MagdaSzut <magda.szut@gmail.com>
2022-11-28 11:13:56 +0100

    5 versions - final solution with big data check & comparison to previous solution with fill array

--- src/main/java/org/example/MaxCounters.java
+++ src/main/java/org/example/MaxCounters.java
@@ -7,8 +7,6 @@ import static java.lang.Math.max;
 public class MaxCounters {
     public static int[] countMax(int n, int[] arr) {
 
-        System.out.println(Arrays.toString(arr));
-
         int[] result = new int[n];
         int max = 0;
         int max_fin = 0;
@@ -25,7 +23,6 @@ public class MaxCounters {
             } else {
                 max_fin = max;
             }
-            System.out.println(Arrays.toString(result) + " max: " + max + " fin_max: " + max_fin);
         }
 
         for (int i = 0; i < n; i++) {
@@ -53,8 +50,29 @@ public class MaxCounters {
         return result;
     }
 
+    public static int[] produceRandomeArray(int range, int size) {
+        int[] array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * range + 1);
+        }
+        return array;
+    }
 
     public static void main(String[] args) {
+
+        //Random big data test-only for effectivness test
+        int[] array = produceRandomeArray(90000, 100000);
+        int len = 10000;
+        long startTime = System.nanoTime();
+        countMax(len, array);
+        long endTime = System.nanoTime();
+        System.out.println(endTime - startTime);
+        startTime = System.nanoTime();
+        countMax2(len, array);
+        endTime = System.nanoTime();
+        System.out.println(endTime - startTime);
+        System.out.println("Big data done");
+        System.out.println();
 
         //Regular tests - MaxCount 100%, MaxCount2 77% effective
         System.out.println(Arrays.toString(countMax(10, new int[]{3, 6, 4, 12, 13, 4, 4, 3, 3, 11, 3, 3, 6, 1})));
