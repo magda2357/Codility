7a80ade4044840e89b2566bf353f8e75dfde2eee
MagdaSzut <magda.szut@gmail.com>
2022-11-28 11:18:06 +0100

    6 & FINAL version

--- src/main/java/org/example/MaxCounters.java
+++ src/main/java/org/example/MaxCounters.java
@@ -28,25 +28,7 @@ public class MaxCounters {
         for (int i = 0; i < n; i++) {
             result[i] = max(max_fin, result[i]);
         }
-        return result;
-    }
 
-    public static int[] countMax2(int n, int[] arr) {
-        int[] result = new int[n];
-        int max = 0;
-
-        for (int j : arr) {
-
-            if (j <= n) {
-                if (result[j - 1] > max) {
-                    max = result[j - 1];
-                }
-                result[j - 1] += 1;
-                max = max(result[j - 1], max);
-            } else {
-                Arrays.fill(result, max);
-            }
-        }
         return result;
     }
 
@@ -60,38 +42,26 @@ public class MaxCounters {
 
     public static void main(String[] args) {
 
-        //Random big data test-only for effectivness test
         int[] array = produceRandomeArray(90000, 100000);
         int len = 10000;
         long startTime = System.nanoTime();
         countMax(len, array);
         long endTime = System.nanoTime();
         System.out.println(endTime - startTime);
-        startTime = System.nanoTime();
-        countMax2(len, array);
-        endTime = System.nanoTime();
-        System.out.println(endTime - startTime);
         System.out.println("Big data done");
         System.out.println();
 
-        //Regular tests - MaxCount 100%, MaxCount2 77% effective
         System.out.println(Arrays.toString(countMax(10, new int[]{3, 6, 4, 12, 13, 4, 4, 3, 3, 11, 3, 3, 6, 1})));
-        System.out.println(Arrays.toString(countMax2(10, new int[]{3, 6, 4, 12, 13, 4, 4, 3, 3, 11, 3, 3, 6, 1})));
         System.out.println();
         System.out.println(Arrays.toString(countMax(5, new int[]{3, 4, 4, 6, 1, 4, 4})));
-        System.out.println(Arrays.toString(countMax2(5, new int[]{3, 4, 4, 6, 1, 4, 4})));
         System.out.println();
         System.out.println(Arrays.toString(countMax(2, new int[]{1, 3})));
-        System.out.println(Arrays.toString(countMax2(2, new int[]{1, 3})));
         System.out.println();
         System.out.println(Arrays.toString(countMax(1, new int[]{1})));
-        System.out.println(Arrays.toString(countMax2(1, new int[]{1})));
         System.out.println();
         System.out.println(Arrays.toString(countMax(1, new int[]{2, 1})));
-        System.out.println(Arrays.toString(countMax2(1, new int[]{2, 1})));
         System.out.println();
         System.out.println(Arrays.toString(countMax(1, new int[]{1, 2})));
-        System.out.println(Arrays.toString(countMax2(1, new int[]{1, 2})));
     }
 
 }
