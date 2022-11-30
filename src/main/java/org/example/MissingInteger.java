989a6f1f0a6cccba565e796e1f36b5a08d4126ed
MagdaSzut <magda.szut@gmail.com>
2022-11-30 22:02:42 +0100

    final solution - 100% effective, using stream for sum

--- src/main/java/org/example/MissingInteger.java
+++ src/main/java/org/example/MissingInteger.java
@@ -6,25 +6,43 @@ public class MissingInteger {
 
     public static int findMissingInteger(int[] array) {
 
-        Arrays.sort(array);
-        int result = array[array.length - 1] + 1;
+        int[] arrayShort = Arrays.stream(array).
+                sorted()
+                .distinct()
+                .filter(a -> a > 0)
+                .toArray();
+        System.out.println("Array: " + Arrays.toString(arrayShort) + ", result: ");
 
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] < 0) {
-                result = 1;
-            } else if ((array[i] - array[i - 1]) > 1) {
-                result = array[i - 1] + 1;
+        int len = arrayShort.length;
+        int result = 1;
+
+        if (len > 0) {
+            if (arrayShort[len - 1] == len) {
+                result = len + 1;
+            } else if (arrayShort[0] == 1) {
+                for (int i = 1; i < len; i++) {
+                    if ((arrayShort[i] - arrayShort[i - 1]) > 1) {
+                        result = arrayShort[i - 1] + 1;
+                        break;
+                    }
+                }
             }
         }
         return result;
-        /*int beginning = Arrays.binarySearch(array, 1);*/
     }
 
     public static void main(String[] args) {
-        System.out.println(findMissingInteger(new int[]{1, 3, 6, 4, 1, 2}));
+
+        System.out.println(findMissingInteger(new int[]{-2, -1, 0, 1, 3, 6, 4, 1, 2}));
         System.out.println(findMissingInteger(new int[]{1, 2, 3}));
+        System.out.println(findMissingInteger(new int[]{4, 5, 6, 2}));
+        System.out.println(findMissingInteger(new int[]{-1}));
+        System.out.println(findMissingInteger(new int[]{0}));
+        System.out.println(findMissingInteger(new int[]{1}));
+        System.out.println(findMissingInteger(new int[]{2}));
         System.out.println(findMissingInteger(new int[]{-1, -3}));
         System.out.println(findMissingInteger(new int[]{1, -3, -6, 4, 1, 2}));
-        System.out.println(findMissingInteger(new int[]{1, -3, 6, -4, 1, 2}));
+        System.out.println(findMissingInteger(new int[]{1, -3, -6, 4, 1, 2}));
+        System.out.println(findMissingInteger(new int[]{1, 3, 6, 4, 1, 2}));
     }
 }
