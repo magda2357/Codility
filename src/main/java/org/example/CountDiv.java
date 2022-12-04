e9e9c5173b83d62a876c69757db3b25020f27801
MagdaSzut <magda.szut@gmail.com>
2022-12-04 05:14:20 +0100

    final version with test, pure solution based on alghoritm

--- src/main/java/org/example/CountDiv.java
+++ src/main/java/org/example/CountDiv.java
@@ -2,57 +2,62 @@ package org.example;
 
 public class CountDiv {
 
-    //    @Test
     public static int returnCountDiv(int a, int b, int k) {
-        int result = Math.round((float) (b - a) / (float) k);
-        if (a == b && (a == 0 || a % k == 0)) {
-            result = 1;
+
+        int result = 0;
+
+        if (a > 0) {
+            result = (b - (b % k)) / k - (a - 1 - (a - 1) % k) / k;
+            if (a == b && a % k == 0) {
+                result = 1;
+            }
+        } else if (a == 0) {
+            result = (b - (b % k)) / k + 1;
         }
+
         return result;
     }
 
-    public static int returnCountDiv3(int a, int b, int k) {
+    public static int returnCountDivInefficient(int a, int b, int k) {
 
-        return (b / k) - (a / k);
-    }
-
-    public static int returnCountDiv2(int a, int b, int k) {
         int result = 0;
+
         for (int i = a; i <= b; i++) {
             if (i % k == 0) {
                 result++;
             }
         }
+
         return result;
     }
 
     public static void main(String[] args) {
         System.out.println(returnCountDiv(10, 10, 5));
-        System.out.println(returnCountDiv2(10, 10, 5));
+        System.out.println(returnCountDivInefficient(10, 10, 5));
         System.out.println(returnCountDiv(10, 10, 7));
-        System.out.println(returnCountDiv2(10, 10, 7));
+        System.out.println(returnCountDivInefficient(10, 10, 7));
         System.out.println(returnCountDiv(10, 10, 20));
-        System.out.println(returnCountDiv2(10, 10, 20));
+        System.out.println(returnCountDivInefficient(10, 10, 20));
         System.out.println(returnCountDiv(0, 0, 11));
-        System.out.println(returnCountDiv2(0, 0, 11));
+        System.out.println(returnCountDivInefficient(0, 0, 11));
         System.out.println(returnCountDiv(1, 1, 11));
-        System.out.println(returnCountDiv2(1, 1, 11));
+        System.out.println(returnCountDivInefficient(1, 1, 11));
         System.out.println(returnCountDiv(6, 11, 2));
-        System.out.println(returnCountDiv2(6, 11, 2));
-        System.out.println(returnCountDiv(1, 100, 20));
-        System.out.println(returnCountDiv2(1, 100, 20));
+        System.out.println(returnCountDivInefficient(6, 11, 2));
         System.out.println(returnCountDiv(11, 345, 17));
-        System.out.println(returnCountDiv2(11, 345, 17));
-        System.out.println(returnCountDiv(1, 14, 2));
-        System.out.println(returnCountDiv2(1, 14, 2));
-        System.out.println(returnCountDiv(1, 14, 3));
-        System.out.println(returnCountDiv2(1, 14, 3));
-//        System.out.println(returnCountDiv(1,2000000000,3));
-//        System.out.println(returnCountDiv2(1,2000000000,3));
-//        System.out.println(returnCountDiv(1,2000000000,2));
-//        System.out.println(returnCountDiv2(1,2000000000,2));
-//        System.out.println(returnCountDiv(1,2000000000,1200000000));
-//        System.out.println(returnCountDiv2(1,2000000000,1200000000));
+        System.out.println(returnCountDivInefficient(11, 345, 17));
+        System.out.println(returnCountDiv(1, 20, 3));
+        System.out.println(returnCountDivInefficient(1, 20, 3));
+        System.out.println(returnCountDiv(1, 20, 2));
+        System.out.println(returnCountDivInefficient(1, 20, 2));
+        System.out.println(returnCountDiv(100, 123456789, 2));
+        System.out.println(returnCountDivInefficient(100, 123456789, 2));
+        System.out.println(returnCountDiv(101, 123456789, 10000));
+        System.out.println(returnCountDivInefficient(101, 123456789, 10000));
+        System.out.println(returnCountDiv(0, 2000000000, 1));
+        System.out.println(returnCountDivInefficient(0, 2000000000, 1));
+        System.out.println(returnCountDiv(0, 2000000000, 2000000000));
+        System.out.println(returnCountDivInefficient(0, 2000000000, 2000000000));
     }
 
 }
