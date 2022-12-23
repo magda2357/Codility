1584cd0d5059595db0a63479d4e7a11ddda24cff
MagdaSzut <magda.szut@gmail.com>
2022-12-24 00:22:46 +0100

    first version 100% with simple cahrs and loops

--- src/main/java/org/example/GenomicRangeQuery.java
+++ src/main/java/org/example/GenomicRangeQuery.java
@@ -1,91 +1,63 @@
 package org.example;
 
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class GenomicRangeQuery {
 
     public static int[] countMinImpactFactor(String S, int[] P, int[] Q) {
 
-        String substring = "";
         int[] result = new int[P.length];
+        int[][] count = new int[S.length()][4];
+
+        for (int i = 0; i < S.length(); i++) {
+            char c = S.charAt(i);
+            if (c == 'A') {
+                count[i][0] = 1;
+            } else if (c == 'C') {
+                count[i][1] = 1;
+            } else if (c == 'G') {
+                count[i][2] = 1;
+            } else if (c == 'T') {
+                count[i][3] = 1;
+            }
+        }
+
+        for (int i = 1; i < S.length(); i++) {
+            for (int j = 0; j < 4; j++) {
+                count[i][j] += count[i - 1][j];
+            }
+        }
 
         for (int i = 0; i < P.length; i++) {
-            substring = Arrays.stream(S.substring(P[i], Q[i] + 1)
-                            .split(""))
-                    .distinct()
-                    .sorted()
-                    .collect(Collectors.joining());
-            substring = String.valueOf(substring.charAt(0));
+            int start = P[i];
+            int end = Q[i];
 
-            switch (substring) {
-                case "A":
-                    result[i] = 1;
+            for (int j = 0; j < 4; j++) {
+                int min = 0;
+
+                if (start - 1 >= 0) {
+                    min = count[start - 1][j];
+                }
+
+                if (count[end][j] - min > 0) {
+                    result[i] = j + 1;
                     break;
-                case "C":
-                    result[i] = 2;
-                    break;
-                case "G":
-                    result[i] = 3;
-                    break;
-                default:
-                    result[i] = 4;
-                    break;
+                }
             }
 
         }
 
         return result;
 
-    }
-
-    public static int[] countMinImpactFactor2(String S, int[] P, int[] Q) {
-
-        String substring = "";
-        String x = "";
-        int min = 4;
-        int[] result = new int[P.length];
-
-        for (int i = 0; i < P.length; i++) {
-
-            substring = S.substring(P[i], Q[i] + 1);
-            min = 4;
-            for (int j = 0; j < Q[i] + 1 - P[i]; j++) {
-                result[i] = 4;
-                x = String.valueOf(substring.charAt(j));
-                switch (x) {
-                    case "A":
-                        min = 1;
-                        break;
-                    case "C":
-                        min = 2;
-                        break;
-                    case "G":
-                        min = 3;
-                        break;
-                }
-                if (result[i] >= min) {
-                    result[i] = min;
-                }
-                if (result[i] == min) {
-                    continue;
-                }
-            }
-        }
-        return result;
     }
 
 
     public static void main(String[] args) {
 
         System.out.println(Arrays.toString(countMinImpactFactor("CAGCCTAACGTAAGTC", new int[]{2, 5, 0}, new int[]{4, 5, 6})));
-        System.out.println(Arrays.toString(countMinImpactFactor2("CAGCCTAACGTAAGTC", new int[]{2, 5, 0}, new int[]{4, 5, 6})));
         System.out.println(Arrays.toString(countMinImpactFactor("CAGCCTAACGTAAGTC", new int[]{2}, new int[]{4})));
-        System.out.println(Arrays.toString(countMinImpactFactor2("CAGCCTAACGTAAGTC", new int[]{2}, new int[]{4})));
         System.out.println(Arrays.toString(countMinImpactFactor("CAGCC", new int[]{2}, new int[]{4})));
-        System.out.println(Arrays.toString(countMinImpactFactor2("CAGCC", new int[]{2}, new int[]{4})));
         System.out.println(Arrays.toString(countMinImpactFactor("GT", new int[]{0, 0, 1}, new int[]{0, 1, 1})));
-        System.out.println(Arrays.toString(countMinImpactFactor2("GT", new int[]{0, 0, 1}, new int[]{0, 1, 1})));
 
     }
 }
